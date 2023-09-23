f956c9c892385a28e0d3a7c49984a72f45e7f61a
Kiran Chikhalikar <kmchikhalikar96@gmail.com>
2023-09-23 23:56:55 +0530

    Commit-2

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,12 @@
-import React from "react";
+
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 
+
 const Body = () => {
+  
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -16,6 +18,8 @@ const Body = () => {
     },
   ]);
 
+  
+
   return (
     <div>
       <RouterProvider router={appRouter} />
@@ -24,3 +28,4 @@ const Body = () => {
 };
 
 export default Body;
+
